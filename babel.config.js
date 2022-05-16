073cbc8f59fbbe0f85c68574925fb6b918b32672
KITI-loubou <919070296@qq.com>
2022-05-16 23:59:10 +0800

    init

---
+++ babel.config.js
@@ -0,0 +1,12 @@
+/*
+ * @Descripttion: free
+ * @version: 1.1
+ * @Author: VLOU
+ * @Date: 2022-05-16 23:51:52
+ * @LastEditors: VLOU
+ * @LastEditTime: 2022-05-16 23:55:33
+ */
+module.exports = {
+  presets: [['@babel/preset-env', { targets: { node: 'current' } }],
+    '@babel/preset-typescript']
+};
